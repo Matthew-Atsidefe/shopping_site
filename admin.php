9023b0f1cc19225f2697e58c1122998df61c8bf2
Matthew-Atsidefe <atsidefe@gmail.com>
2021-01-05 10:25:18 +0000

    Admin Login

--- admin.php
+++ admin.php
@@ -1,6 +1,15 @@
 <?php 
 	session_start();
 
+	//if login form has been submitted, check if username and password match
+	if(isset($_POST['login'])){
+		$login_sql="SELECT * FROM user WHERE username=".$_POST['username']."'AND password='".sha1($_POST['password'])."'";
+		if($login_query=mysqli_query($dbconnect, $login_sql)){
+			$login_rs=mysqli_fetch_assoc($login_query);
+			$_SESSION['admin']=$login_rs['username'];
+		}
+	}
+
 	if (isset ($_SESSION['admin'])) {
 		include("cpanel.php");
 	} else {
